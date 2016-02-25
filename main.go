d66e54189b65c965c9de3214fe6fd533cb93b0ba
Jeromy <jeromyj@gmail.com>
2016-02-25 09:41:47 -0800

    add watcher for blog page

--- main.go
+++ main.go
@@ -18,32 +18,42 @@ var (
 		Namespace: "ipfs",
 		Help:      "time it takes to get /version on gateways",
 	})
+
+	blog_g = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "blog_get",
+		Subsystem: "ext_watcher",
+		Namespace: "ipfs",
+		Help:      "time it takes to get blog.ipfs.io",
+	})
 )
 
 func init() {
 	prometheus.MustRegister(vers_g)
+	prometheus.MustRegister(blog_g)
+}
+
+func monitorHttpEndpoint(g prometheus.Gauge, url string, interval time.Duration) {
+	for range time.Tick(interval) {
+		before := time.Now()
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+		took := time.Now().Sub(before)
+		g.Add(took.Seconds())
+	}
 }
 
 func main() {
 	addr := flag.String("addr", ":9999", "address to serve metrics on")
 	flag.Parse()
 
-	go func() {
-		for range time.Tick(time.Second * 5) {
-			before := time.Now()
-			resp, err := http.Get("https://ipfs.io/version")
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			io.Copy(ioutil.Discard, resp.Body)
-			resp.Body.Close()
-			took := time.Now().Sub(before)
-			vers_g.Add(took.Seconds())
-		}
-	}()
+	go monitorHttpEndpoint(vers_g, "https://ipfs.io/version", time.Second*5)
+	go monitorHttpEndpoint(blog_g, "https://blog.ipfs.io", time.Second*30)
 
 	http.Handle("/metrics", prometheus.Handler())
-
 	http.ListenAndServe(*addr, nil)
 }
